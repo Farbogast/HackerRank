d694c8d21355cd25ec109080e7a58e1ff969f595
Farbogast <arbogast.francois@gmail.com>
2020-10-20 17:06:56 +0100

    8 challenges ended

---
+++ 10_Days_Of_Javascript/5.Throw.js
@@ -0,0 +1,14 @@
+function isPositive(a) {
+    if (a>0)
+   {
+       return 'YES'
+    } 
+   else if (a===0)
+   {
+       return 'Zero Error'
+    }
+   else if(a<0)
+   {
+       return 'Negative Error'
+    }
+}
